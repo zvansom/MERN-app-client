52e8e096f54fa9544e76f403ae5d110d2221d7d5
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-09-18 06:36:37 -0700

    Merge with mern-token boilerplate.

---
+++ src/auth/Signup.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import SERVER_URL from '../constants/server';
+
+import FormContainer from '../landingPage/FormContainer';
+
+class Signup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  handleNameChange = (e) => { this.setState({ name: e.target.value }); }
+
+  handleEmailChange = (e) => { this.setState({ email: e.target.value }); }
+
+  handlePasswordChange = (e) => { this.setState({ password: e.target.value }); }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    axios.post(SERVER_URL + '/auth/signup', this.state)
+    .then(result => {
+      console.log('SUCCESS!', result);
+      // Add the newly received token to LS
+      localStorage.setItem('mernToken', result.data.token);
+      // Update the user with a call to App.js
+      this.props.updateUser();
+    })
+    .catch(err => {
+      console.log('ERROR', err);
+    });
+  }
+
+  render() {
+    if(this.props.user){
+      return (<Redirect to="/profile" />);
+    }
+    return(
+        <div>
+          <h2>Signup as a new user</h2>
+        
+        </div>
+      );
+  }
+}
+
+export default Signup;
