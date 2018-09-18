24bd550b6c6d274f313035eb9bcb1dd422a0d959
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-09-18 06:36:37 -0700

    Merge with mern-token boilerplate.

---
+++ src/layout/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return(
+        <div>
+          <footer className="footer">
+            <span className="footer-text">
+              BZY &copy; {new Date().getFullYear()}
+            </span>
+          </footer>
+        </div>
+      );
+  }
+}
+
+export default Footer;
