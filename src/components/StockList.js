3f209d293745f78b092636c7c4ac81f173f1f6e7
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-19 11:25:13 -0700

    Changed all divs to more semantically appropriate tags.

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -5,9 +5,9 @@ import SubmitButton from "./SubmitButton";
 import LineChart from "../chart/LineChart";
 import Toggle from './Toggle';
 
-// import { arrowDown } from "react-icons-kit/fa/arrowDown";
-// import { arrowUp } from "react-icons-kit/fa/arrowUp";
-// import { Icon } from "react-icons-kit";
+import { arrowDown } from "react-icons-kit/fa/arrowDown";
+import { arrowUp } from "react-icons-kit/fa/arrowUp";
+import { Icon } from "react-icons-kit";
 
 export default class StockList extends Component {
   state = {
@@ -15,43 +15,44 @@ export default class StockList extends Component {
     up: Math.round(Math.random() * 100) / 100,
   };
 
-  // async componentDidMount() {
-  //   const url = `https://api.iextrading.com/1.0/stock/${
-  //     this.props.symbol
-  //   }/ohlc`;
-  //   const response = await fetch(url);
-  //   const parse = await response.json();
+  async componentDidMount() {
+    const url = `https://api.iextrading.com/1.0/stock/${
+      this.props.symbol
+    }/ohlc`;
+    const response = await fetch(url);
+    const parse = await response.json();
 
-  //   this.setState({
-  //     price: parse.close.price
-  //   });
-  // }
+    this.setState({
+      price: parse.close.price
+    });
+  }
 
   render() {
     let arrow;
     if (this.state.up > 0.5) {
       arrow = (
         //TODO: hardcoded pewrcentages
-        <div style={{ color: "#4040a1" }}>
+        <td style={{ color: "#4040a1" }}>
           +{this.state.up * 100}%
-          {/* <Icon size={25} icon={arrowUp} /> */}
-        </div>
+          <Icon size={25} icon={arrowUp} />
+        </td>
       );
     } else {
       arrow = (
-        <div style={{ color: "#c83349" }}>
+        <td style={{ color: "#c83349" }}>
           -{this.state.up * 100}%
-          {/* <Icon size={25} icon={arrowDown} /> */}
-        </div>
+          <Icon size={25} icon={arrowDown} />
+        </td>
       );
     }
 
     return (
-      <StockItem>
-        <p>{this.props.name}</p>
-        <p>{this.state.price}</p>
+      <StyledTr>
+        <td>{this.props.name}</td>
+        <td>{this.state.price}</td>
         {arrow}
-        <InputField
+        <td>
+          <InputField
           name={"shares"}
           type={"number"}
           placeholder={"0"}
@@ -59,27 +60,29 @@ export default class StockList extends Component {
           min={"0"}
           max={"100"} //TODO: this should be based on your available funds
           step={"1"}
-        />
-        <div id="submit-containers">
+          />
+        </td>
+        <td id="submit-containers">
           <SubmitButton value="Sell" fillColor="#ff6f69" />
           <SubmitButton value="Buy" fillColor="#c83349" />
-        </div>
-          <Toggle>
-          {({ on, toggle }) => (
-            <div>
-              <button onClick={toggle}>Show</button>
-              {on && <LineChart symbol={this.props.symbol} />}
-            </div>
-          )}
-          </Toggle>
-      </StockItem>
+        </td>
+        <Toggle>
+        {({ on, toggle }) => (
+          <div>
+            <button onClick={toggle}>Show</button>
+            {on && <LineChart symbol={this.props.symbol} />}
+          </div>
+        )}
+        </Toggle>
+      </StyledTr>
     );
   }
 }
 
-const StockItem = styled.div`
+const StyledTr = styled.tr`
+
+  > td {
   border: 1px solid black;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 10px;
+
+  }
 `;
