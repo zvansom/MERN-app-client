39b04add5aafb13feb1dd711c026bf974aa5cd03
yinebeb Girma <yinebebgirma@yinebebs-MacBook-Pro.local>
2018-09-20 16:03:38 -0700

    sell and buy logic accomodate arrays of obj

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import SubmitButton from "./SubmitButton";
 import ShareAmount from "./ShareAmount";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export default class Trade extends Component {
   constructor(props) {
     super(props);
     this.state = {
       shares: 0,
-      portfolio: [{ abc: 15, www: 20 }], //TODO: this should be actual user data if existing user
-      workingCaptal: 100000, //TODO: this should be actual user data if existing user
-      sharePrice: 1000, //TODO: this should be actual user data if existing user
-      company: "abc" //TODO: this should be actual user data if existing user
+      portfolio: this.props.portfolio,
+      workingCaptal: 100000,
+      sharePrice: this.props.currentPrice,
+      symbol: this.props.symbol
     };
   }
 
@@ -23,39 +23,56 @@ export default class Trade extends Component {
   hundleSell = e => {
     e.preventDefault();
     // Update user potfolio
+
     let shares = this.state.shares;
-    let newPortfolio = this.state.portfolio[0] || {};
-    let totalShares = newPortfolio[this.state.company] - shares;
-    newPortfolio[this.state.company] = totalShares;
-    // Update user working capital
-    let totalGain = this.state.sharePrice * shares;
-    let newWorkingCapital = this.state.workingCaptal + totalGain;
-    this.setState({
-      portfolio: newPortfolio,
-      workingCaptal: newWorkingCapital
+    console.log("sharePrice", this.state.sharePrice);
+
+    let newPortfolio = this.state.portfolio || [{}];
+    let newWorkingCapital;
+    newPortfolio.forEach(stock => {
+      if (stock.symbol === this.state.symbol) {
+        stock.numShares -= shares;
+        // Update user working capital
+        let totalGain = this.state.sharePrice * shares;
+        newWorkingCapital = this.state.workingCaptal + totalGain;
+        this.setState({
+          portfolio: newPortfolio,
+          workingCaptal: newWorkingCapital
+        });
+        console.log("portfolio", this.state.portfolio);
+        console.log("workingCaptal", newWorkingCapital);
+      }
     });
-    console.log("portfolio", this.state.portfolio);
-    console.log("workingCaptal", newWorkingCapital);
   };
 
   hundleBuy = e => {
     e.preventDefault();
     // Update user potfolio
+    console.log("portfolio", this.state.portfolio);
+    console.log("workingCaptal", this.state.workingCaptal);
     let shares = this.state.shares;
-    let newPortfolio = this.state.portfolio[0] || {};
-    let totalShares = newPortfolio[this.state.company]
-      ? newPortfolio[this.state.company] + shares
-      : shares;
-    newPortfolio[this.state.company] = totalShares;
-    // Update user working capital
-    let totalSpend = this.state.sharePrice * shares;
-    let newWorkingCapital = this.state.workingCaptal - totalSpend;
+    let newPortfolio = this.state.portfolio || [{}];
+    let newWorkingCapital;
+    let found = false;
+    newPortfolio.forEach(stock => {
+      if (stock.symbol === this.state.symbol) {
+        stock.numShares += shares;
+        // Update user working capital
+        let totalSpend = this.state.sharePrice * shares;
+        newWorkingCapital = this.state.workingCaptal - totalSpend;
+        found = !found;
+      }
+    });
+    if (found) {
+      newPortfolio[this.state.symbol] = shares;
+      let totalSpend = this.state.sharePrice * shares;
+      newWorkingCapital = this.state.workingCaptal - totalSpend;
+    }
+
     this.setState({
       portfolio: newPortfolio,
       workingCaptal: newWorkingCapital
     });
-    console.log("workingCaptal", this.state.workingCaptal);
-    console.log("portfolio", this.state.portfolio);
   };
 
   render() {
@@ -70,7 +87,7 @@ export default class Trade extends Component {
             <SubmitButton
               value="Sell"
               fillColor="#ff6f69"
-              onClick={this.hundleSell}
+              handleClick={this.hundleSell}
             />
             <ShareAmount max={sellmax} handleshares={this.handleshares} />
           </div>
@@ -79,7 +96,7 @@ export default class Trade extends Component {
           <SubmitButton
             value="Buy"
             fillColor="#c83349"
-            onClick={this.hundleBuy}
+            handleClick={this.hundleBuy}
           />
           <ShareAmount max={buymax} handleshares={this.handleshares} />
         </div>
