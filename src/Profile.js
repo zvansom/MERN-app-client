0f3a51895e529f13b36f99f627dac41854cfd473
yinebeb Girma <yinebebgirma@yinebebs-MacBook-Pro.local>
2018-09-20 12:55:43 -0700

    add async for portfolio value calculation

--- src/Profile.js
+++ src/Profile.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 // Import components
 import ProgressBar from "./components/ProgressBar";
 import StockTable from "./components/StockTable";
 import LineChart from "./chart/LineChart";
 import Trade from "./components/Trade";
-
-var async = require('async');
+import async from "async";
 
 const SAMPLE_PORTFOLIO = [
   {
-    symbol: 'ATVI',
+    symbol: "ATVI",
     numShares: 4,
-    purchasePrice: null,
+    purchasePrice: null
   },
   {
-    symbol: 'GOOG',
+    symbol: "GOOG",
     numShares: 4,
-    purchasePrice: null,
+    purchasePrice: null
   }
-]
+];
 
 class Profile extends Component {
   constructor(props) {
@@ -28,43 +27,45 @@ class Profile extends Component {
     this.state = {
       workingCap: null,
       currentPrice: null,
-      activeSymbol: 'ATVI',
+      activeSymbol: "ATVI"
     };
   }
 
   calculatePortfolioValue = portfolio => {
     let portfolioValue;
     //const symbols = portfolio.map(stock => stock.symbol);
-    async.forEach(portfolio, function(stock,done){
-      const url = `https://api.iextrading.com/1.0/stock/${
-        stock.symbol
-      }/ohlc`;
-      fetch(url)
-        .then(response => response.json())
-        .then(parse => {stock.purchasePrice=parse.close.price});
-    }, function (){
-      portfolioValue = portfolio.reduce((acc, stock) => {
-        return acc += (stock.purchasePrice * stock.numShares);
-      }, 0)
-      console.log('portfolioValue', portfolioValue);
-    })
-  }
-
-      
-
-
+    async.forEach(
+      portfolio,
+      function(stock, done) {
+        const url = `https://api.iextrading.com/1.0/stock/${stock.symbol}/ohlc`;
+        fetch(url)
+          .then(response => response.json())
+          .then(parse => {
+            stock.purchasePrice = parse.close.price;
+            done();
+          });
+      },
+      function() {
+        portfolioValue = portfolio.reduce((acc, stock) => {
+          return (acc += stock.purchasePrice * stock.numShares);
+        }, 0);
+        console.log("portfolio", portfolio);
+        console.log("portfolioValue", portfolioValue);
+      }
+    );
+  };
 
   handleClick = e => {
     console.log(e.target.dataset.currentprice);
-    this.setState({ 
+    this.setState({
       activeSymbol: e.target.dataset.symbol,
-      currentPrice: e.target.dataset.currentprice,
-     });
+      currentPrice: e.target.dataset.currentprice
+    });
   };
 
   render() {
-    this.calculatePortfolioValue(SAMPLE_PORTFOLIO)
-    console.log('SAMPLE_PORTFOLIO', SAMPLE_PORTFOLIO)
+    this.calculatePortfolioValue(SAMPLE_PORTFOLIO);
+    console.log("SAMPLE_PORTFOLIO", SAMPLE_PORTFOLIO);
     if (this.props.user) {
       const { workingCapital, portfolio } = this.props;
       const { activeSymbol, currentPrice } = this.state;
