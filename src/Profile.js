181bd1561d8d074be045a7c0bcf3289737e808cd
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-20 15:15:12 -0700

    Passing user to trade component

--- src/Profile.js
+++ src/Profile.js
@@ -55,7 +55,7 @@ class Profile extends Component {
         <div>
           <h2>Current Portfolio Value: ${this.state.portfolioValue.toFixed(2)}</h2>
           <LineChart symbol={activeSymbol} />
-          <Trade currentPrice={currentPrice} symbol={activeSymbol} />
+          <Trade user={this.props.user} currentPrice={currentPrice} symbol={activeSymbol} />
           <h2>Buy some new stocks!</h2>
           <StockTable handleClick={this.handleClick} />
         </div>
