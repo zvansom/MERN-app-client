13c4dfa444a331de779c86b330815ffd71b54787
yinebeb Girma <yinebebgirma@yinebebs-MacBook-Pro.local>
2018-09-21 14:24:32 -0700

    profile cahnges

--- src/Profile.js
+++ src/Profile.js
@@ -189,3 +189,39 @@ function calculatePortfolio(
 
   return [newPortfolio, newCapital];
 }
+
+// ///// Helpers
+// function calculatePortfolio2(
+//   trade,
+//   shares,
+//   symbol,
+//   newPortfolio,
+//   tradeValue,
+//   newCapital
+// ) {
+//   var ownedShares = newPortfolio.find(function(stock) {
+//     return stock.symbol === symbol;
+//   });
+
+//   let sell = trade === "Sell";
+//   newCapital = sell ? (newCapital += tradeValue) : (newCapital -= tradeValue);
+//   let newNumShares = sell
+//     ? (ownedShares.numShares -= shares)
+//     : ownedShares
+//       ? (ownedShares.numShares += shares)
+//       : shares;
+
+//   if (ownedShares) {
+//     ownedShares.numShares = newNumShares;
+//   } else {
+//     newPortfolio.push({
+//       symbol: symbol,
+//       numShares: shares
+//     });
+//   }
+
+//   newPortfolio = newPortfolio.filter(function(stock) {
+//     return stock.numShares !== 0;
+//   });
+//   return [newPortfolio, newCapital];
+// }
