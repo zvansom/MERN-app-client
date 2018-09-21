a54333ff43703d31b409b8746ed34e04a6835b09
yinebeb Girma <yinebebgirma@yinebebs-MacBook-Pro.local>
2018-09-21 10:48:58 -0700

    check user login before render

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -5,82 +5,39 @@ import styled from "styled-components";
 import SelectTrade from "./SelectTrade";
 
 export default class Trade extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      trade: "",
-      shares: 0,
-      sellMax: "",
-      buyMax: ""
-    };
-  }
-
-  handleShares = e => {
-    e.preventDefault();
-    this.setState({ shares: e.target.value });
-    // console.log(this.state.shares);
-  };
-
-  handleTradeSelection = e => {
-    this.setState({ trade: e.target.value });
-    // console.log("trade", this.state.trade);
-  };
-
-  hundleTrade = e => {
-    e.preventDefault();
-
-    let trade = this.state.trade;
-    if (!trade) {
-      return;
-    }
-    let shares = Number(this.state.shares);
-    let symbol = this.props.symbol;
-    let tradeValue = this.props.currentPrice * shares;
-    let newPortfolio = [...this.props.portfolio] || [{}];
-    let newCapital = this.props.workingCapital;
-    [newPortfolio, newCapital] = calculatePortfolio(
-      trade,
-      shares,
-      symbol,
-      newPortfolio,
-      tradeValue,
-      newCapital
-    );
-    this.setState({
-      portfolio: newPortfolio,
-      workingCaptal: newCapital,
-      shares: 0,
-      trade: ""
-    });
-
-    console.log("newPortfolio", newPortfolio);
-    console.log("newWorkingCapital", newCapital);
-    console.log("traded shares", this.state.shares);
-  };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     sellMax: "",
+  //     buyMax: ""
+  //   };
+  // }
 
   render() {
     // TODO : Pass these props from user data
-    const { portfolio, symbol } = this.props;
+    const {
+      portfolio,
+      symbol,
+      currentPrice,
+      workingCapital,
+      trade,
+      shares
+    } = this.props;
     let tradeArray;
     let buyMax;
-    var ownedShares = this.props.portfolio.find(function(stock) {
+    const ownedShares = portfolio.find(function(stock) {
       return stock.symbol === symbol;
     });
 
     let sellMax = ownedShares ? ownedShares.numShares : "";
     if (sellMax) {
-      buyMax = Math.floor(this.props.workingCapital / this.props.currentPrice);
+      buyMax = Math.floor(workingCapital / currentPrice);
       tradeArray = ["Buy", "Sell"];
     } else {
       buyMax = 100; //TODO: we need actual values on load??
       tradeArray = ["Buy"];
     }
-    let max =
-      this.state.trade === ""
-        ? 0
-        : this.state.trade === "Sell"
-          ? sellMax
-          : buyMax;
+    let max = trade === "" ? 0 : trade === "Sell" ? sellMax : buyMax;
     // console.log("trade", this.state.trade);
     // console.log("tradeArray", tradeArray);
     // console.log("max", max);
@@ -94,23 +51,21 @@ export default class Trade extends Component {
               <SelectTrade
                 name={"trade"}
                 placeholder={"Trade:"}
-                controlFunc={this.handleTradeSelection}
+                controlFunc={this.props.handleTradeSelection}
                 options={tradeArray}
-                selectedOption={this.state.trade}
+                selectedOption={trade}
               />
             </td>
             <td>
-              <ShareAmount max={max} handleshares={this.handleShares} x />
+              <ShareAmount max={max} handleshares={this.props.handleShares} x />
             </td>
-            <td>
-              $ {(this.state.shares * this.props.currentPrice).toFixed(2)}
-            </td>
+            <td>$ {(shares * currentPrice).toFixed(2)}</td>
             <td>
               {" "}
               <SubmitButton
-                value={this.state.trade || "Trade"}
-                fillColor={this.state.trade ? "#405d27" : "#d5e1df"}
-                handleClick={this.hundleTrade}
+                value={trade || "Trade"}
+                fillColor={trade ? "#405d27" : "#d5e1df"}
+                handleClick={this.props.hundleTrade}
               />
             </td>
           </tr>
@@ -127,37 +82,3 @@ const Table = styled.table`
     padding: 6px;
   }
 `;
-
-// Helpers
-function calculatePortfolio(
-  trade,
-  shares,
-  symbol,
-  newPortfolio,
-  tradeValue,
-  newCapital
-) {
-  if (trade === "Sell") {
-    newCapital += tradeValue;
-    newPortfolio.forEach(stock => {
-      if (stock.symbol === symbol) {
-        stock.numShares -= shares;
-      }
-    });
-  } else {
-    newCapital -= tradeValue;
-    var ownedShares = newPortfolio.find(function(stock) {
-      return stock.symbol === symbol;
-    });
-
-    let newSahres = ownedShares
-      ? (ownedShares.numShares += shares)
-      : {
-          symbol: symbol,
-          numShares: shares
-        };
-
-    newPortfolio.push(newSahres);
-  }
-  return [newPortfolio, newCapital];
-}
