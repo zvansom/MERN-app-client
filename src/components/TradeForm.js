03d9f81465de40396b7480e41a1c72021dd7a475
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-22 12:33:22 -0700

    All child components now written inline

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import SubmitButton from "./SubmitButton";
-import ShareAmount from "./ShareAmount";
 import styled from "styled-components";
 
 export default class TradeForm extends Component {
@@ -9,35 +7,35 @@ export default class TradeForm extends Component {
 
     return (
       <StyledTradeForm>
-        <select className="form-select">
-          <option value="Buy">Buy</option>
-          <option value="Sell">Sell</option>
-        </select>
-        <ShareAmount
-          max={max}
-          shares={this.props.shares}
-          decreaseShares={this.props.decreaseShares}
-          incrementShares={this.props.incrementShares}
-        />
-      {(shares * currentPrice).toFixed(2)}
+        <div className="form-element">
+          <select className="form-select">
+            <option value="Buy">Buy</option>
+            <option value="Sell">Sell</option>
+          </select>
+        </div>
+        <div className="form-element">
+          <button> - </button>
+          <p>{this.props.shares}</p>
+          <button> + </button>
+        </div>
+        {(shares * currentPrice).toFixed(2)}
         {" "}
-        <SubmitButton
-          value={trade || "Trade"}
-          fillColor={trade ? "#405d27" : "#d5e1df"}
-          handleClick={this.props.hundleTrade}
-        />
+        <input type="submit" value="Trade" />
       </StyledTradeForm>
     );
   }
 }
 
 const StyledTradeForm = styled.form`
+  display: grid;
+  gap: 20px;
+  grid-auto-flow: column;
   color: #000;
-  display: inline-block;
   background: #fff;
-  border-radius: 20px;
-  border-spacing: 10px;
-  > td {
-    padding: 6px;
+  > .form-element {
+    display: inline-block;
+    > p {
+      display: inline;
+    }
   }
 `;
